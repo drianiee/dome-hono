358079dbabb47a93ae2c5f52336d9186b5ccddd3
aldrxxn <andriantambunan2752@gmail.com>
2025-01-02 18:23:28 +0700

    add: vercel.json

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import { errorHandler } from './middleware/errorHandler';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 const data = require('./src/utils/units.json');
 
